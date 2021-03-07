e7145d9dc13cb1122553d1f6a37507c3435c3730
johnmetronome <johnmetronome@gmail.com>
2021-03-07 10:05:39 +0100

    Up

---
+++ furniture 1 N&B.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="furniture 1 N&amp;B" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="furniture 1 N&amp;B.png" width="320" height="320"/>
+</tileset>
