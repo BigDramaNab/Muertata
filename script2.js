26b9267bbf55026dce79c0a297e1aa8b1ee98324
BigDramaNab <79466307+BigDramaNab@users.noreply.github.com>
2021-04-16 18:36:25 +0200

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,3 +1,18 @@
 WA.onEnterZone('violetta', () => {
     WA.sendChatMessage("Hello!", 'Afred');
 })
+
+WA.onEnterZone('iris', () => {
+    WA.disablePlayerControls();
+    WA.openPopup("popupRectangle", 'Merci de lire ce message message!', [{
+        label: "J'ai lu",
+        className: "primary",
+        callback: (popup) => {
+            WA.restorePlayerControls();
+            popup.close();
+        }
+    }]);
+});
+
+
+WA.sendChatMessage('Bonjour et bienvenue dans la maison !', 'Alfred');
