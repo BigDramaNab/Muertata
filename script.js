a63d3d61cb7fcd7e0ea4c2fc29efe9f46bef3778
BigDramaNab <79466307+BigDramaNab@users.noreply.github.com>
2021-04-17 14:43:01 +0000

    Deploying to gh-pages from master @ 6896c0785fc563fa17eb7461667fbae41d4bcc7a 🚀

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-WA.sendChatMessage('Bienvenue dans la maison.Toute la famille est actuellement au commissariat.', 'Alfred');
+WA.sendChatMessage('Bienvenue', 'Alfred');
 
 WA.onEnterZone('test', () => {
     WA.sendChatMessage("Hello!", 'Mr Robot');
+    WA.disablePlayerControls();
 })
