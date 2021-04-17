ba76d08d277e57f172e497118f8cf9a4cf829584
BigDramaNab <79466307+BigDramaNab@users.noreply.github.com>
2021-04-17 15:10:09 +0000

    Deploying to gh-pages from master @ 97c55369a95144fc96ea4689d16324f12a7480b5 🚀

--- script.js
+++ script.js
@@ -3,21 +3,3 @@ WA.sendChatMessage('Bienvenueee', 'Alfred');
 WA.onEnterZone('test', () => {
     WA.sendChatMessage('what', 'Alfred');
 })
-
-// Open the popup when we enter a given zone
-helloWorldPopup = WA.onEnterZone('test', () => {
-    WA.openPopup("popupRectangle", 'Hello world!', [{
-        label: "Close",
-        className: "primary",
-        callback: (popup) => {
-            // Close the popup when the "Close" button is pressed.
-            popup.close();
-        }
-    });
-}]);
-
-// Close the popup when we leave the zone.
-WA.onLeaveZone('alfred', () => {
-    helloWorldPopup.close();
-});
-
