d51b2c48bf04feb78b8f9fced7055151f7dea689
BigDramaNab <79466307+BigDramaNab@users.noreply.github.com>
2021-04-17 16:31:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-WA.sendChatMessage('Hello world', 'Alfred');
+WA.sendChatMessage('Bienvenue dans la maison.Toute la famille est actuellement au commissariat. Dailleurs, lun dentre eux voudrait vous contacter par téléphone : rendez-vous tous immédiatement dans la pièce centrale de la maison pour lui parler, avant de partir à la recherche dindices.', 'Alfred');
 
-
-
-
-    
-    
-    WA.onEnterZone('violetta', () => {
-    WA.disablePlayerControls();
+WA.onEnterZone('alfred', () => {
+    WA.sendChatMessage("Hello!", 'Mr Robot');
 })
