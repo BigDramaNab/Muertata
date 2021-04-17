a828b766cfe25b0988ab232bebaf629be38ccb79
BigDramaNab <79466307+BigDramaNab@users.noreply.github.com>
2021-04-17 14:39:21 +0000

    Deploying to gh-pages from master @ cb76c2716bb038778e996096da77653be33ac27c 🚀

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-WA.sendChatMessage('Bienvenue dans la maison.Toute la famille est actuellement au commissariat. Dailleurs, lun dentre eux voudrait vous contacter par téléphone : rendez-vous tous immédiatement dans la pièce centrale de la maison pour lui parler, avant de partir à la recherche dindices.', 'Alfred');
+WA.sendChatMessage('Bienvenue dans la maison.Toute la famille est actuellement au commissariat.', 'Alfred');
 
-WA.onEnterZone('alfred', () => {
+WA.onEnterZone('test', () => {
     WA.sendChatMessage("Hello!", 'Mr Robot');
 })
