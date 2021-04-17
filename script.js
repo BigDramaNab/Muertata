c07606e2b625851a27642b9bd830a7c82afd91dd
BigDramaNab <79466307+BigDramaNab@users.noreply.github.com>
2021-04-17 16:24:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,19 +3,8 @@ WA.sendChatMessage('Hello world', 'Alfred');
 
 
 
-// Open the popup when we enter a given zone
-helloWorldPopup = WA.onEnterZone('violetta', () => {
-    WA.openPopup("popup", 'Hello world!', [{
-        label: "Close",
-        className: "primary",
-        callback: (popup) => {
-            // Close the popup when the "Close" button is pressed.
-            popup.close();
-        }
-    });
-}]);
-
-// Close the popup when we leave the zone.
-WA.onLeaveZone('violetta', () => {
-    helloWorldPopup.close();
-});
+    
+    
+    WA.onEnterZone('violetta', () => {
+    WA.disablePlayerControls();
+})
