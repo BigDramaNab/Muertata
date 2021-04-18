f62d34495e114c06bb3456fee8cee7cd3b2483be
johnmetronome <80063998+johnmetronome@users.noreply.github.com>
2021-04-18 15:39:53 +0000

    Deploying to gh-pages from master @ 30821140b856e6973402c3b51e0100585fb73da8 🚀

---
+++ script TOUJOURS OK.js
@@ -0,0 +1,9 @@
+WA.sendChatMessage('TEST SCRIPT JS DE 17H28', 'PIVOT');
+
+WA.onEnterZone('ZONETEST3', () => {
+WA.sendChatMessage("Hello! ZONE TEST 3", 'Alfred');
+})
+
+WA.onLeaveZone('ZONETEST3', () => {
+    WA.sendChatMessage("Goodbye tardif! ", 'Alfred');
+})
