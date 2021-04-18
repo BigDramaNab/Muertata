6291cbf4a342b404297b13b8bd8e5b7d1681f605
johnmetronome <80063998+johnmetronome@users.noreply.github.com>
2021-04-18 16:20:48 +0000

    Deploying to gh-pages from master @ 1d723c8d13f4dd8ac8d9587ccd41d4bb164a1b62 🚀

--- script.js
+++ script.js
@@ -23,3 +23,12 @@ WA.sendChatMessage("Tu as le droit d'aller la", 'Alfred');
 WA.onLeaveZone('ZONETEST3', () => {
     WA.sendChatMessage("Goodbye tardif! ", 'Alfred');
 })
+
+var zoneName = "popUpGoToPageZone";
+var urlPricing = "https://workadventu.re/pricing";
+var urlGettingStarted = "https://workadventu.re/getting-started";
+var isCoWebSiteOpened =  false;
+
+WA.onChatMessage((message => {
+    WA.sendChatMessage('Poly Parrot says: "'+message+'"', 'Poly Parrot');
+}));
