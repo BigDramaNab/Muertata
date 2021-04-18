86fad6ca67c46bfb68b217d0deb6f923fe1ab9a0
johnmetronome <80063998+johnmetronome@users.noreply.github.com>
2021-04-18 18:50:40 +0000

    Deploying to gh-pages from master @ 36faae2c69239fc610ec49e93881d7cb111a09f2 🚀

--- script.js
+++ script.js
@@ -14,22 +14,3 @@ WA.onEnterZone('ZONETEST', () => {
     }]);
 });
 
-
-let helloWorldPopup;
-
-// Open the popup when we enter a given zone
-helloWorldPopup = WA.onEnterZone('ZONETEST', () => {
-    WA.openPopup("myPopup", 'Hello world!', [{
-        label: "Close",
-        className: "primary",
-        callback: (popup) => {
-            // Close the popup when the "Close" button is pressed.
-            popup.close();
-        }
-    });
-};
-
-// Close the popup when we leave the zone.
-WA.onLeaveZone('ZONETEST', () => {
-    helloWorldPopup.close();
-});
