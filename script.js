15661d9f24265233a601cc446a349965ff9a821e
johnmetronome <80063998+johnmetronome@users.noreply.github.com>
2021-04-18 17:17:37 +0000

    Deploying to gh-pages from master @ 744ebd7f5f9d5cfdce235a3d8529b60dfd16800d 🚀

--- script.js
+++ script.js
@@ -1,85 +1,19 @@
-WA.sendChatMessage('TEST SCRIPT JS DE 18H47', 'PIVOT');
+WA.sendChatMessage('TEST SCRIPT JS DE 19H14', 'PIVOT');
 
-
-console.log('SCRIPT LAUNCHED');
-WA.sendChatMessage('Hi, my name is Poly and I repeat what you say!', 'Poly Parrot');
-var isFirstTimeTuto = false;
-var textFirstPopup = 'Hey ! This is how to open start a discussion with someone ! You can be 4 max in a booble';
-var textSecondPopup = 'You can also use the chat to communicate ! ';
-var targetObjectTutoBubble ='tutoBobble';
-var targetObjectTutoChat ='tutoChat';
-var popUpExplanation = undefined;
-function launchTuto (){
-        WA.openPopup(myPopup1, textFirstPopup, [
-            {
-                label: "Next",
-                className: "popUpElement",
-                callback: (popup) => {
-                    popup.close();
-
-                    WA.openPopup(myPopup2, textSecondPopup, [
-                        {
-                            label: "Open Chat",
-                            className: "popUpElement",
-                            callback: (popup1) => {
-                                WA.sendChatMessage("Hey you can talk here too ! ", 'WA Guide');
-                                popup1.close();
-                                WA.restorePlayerControl();
-                            }
-                        }
-
-                    ])
-                }
-            }
-        ]);
-        WA.disablePlayerControl();
-
-}
-WA.onChatMessage((message => {
-    console.log('CHAT MESSAGE RECEIVED BY SCRIPT');
-    WA.sendChatMessage('Poly Parrot says: "'+message+'"', 'Poly Parrot');
-}));
-
-WA.onEnterZone('myTrigger', () => {
-    WA.sendChatMessage("Don't step on my carpet!", 'Poly Parrot');
-})
-
-WA.onLeaveZone('ZONETEST', () => {
-})
-
-WA.onEnterZone('notExist', () => {
-    WA.sendChatMessage("YOU SHOULD NEVER SEE THIS", 'Poly Parrot');
-})
 
 WA.onEnterZone('ZONETEST', () => {
-    WA.displayBubble();
-    if (!isFirstTimeTuto) {
-        isFirstTimeTuto = true;
-        launchTuto();
-    }
-     else popUpExplanation =  WA.openPopup(targetObjectTutoChat,'Do you want to review the explanation ? ', [
-        {
-            label: "No",
-            className: "popUpElementReviewexplanation",
-            callback: (popup) => {
-                popup.close();
-            }
-        },
-        {
-            label: "Yes",
-            className: "popUpElementReviewexplanation",
-            callback: (popup) => {
-                popup.close();
-                launchTuto();
-            }
+    WA.disablePlayerControls();
+    WA.openPopup("myPopup1", 'NIQUE TOI TA MERE PUTAIN', [{
+        label: "Got it!",
+        className: "primary",
+        callback: (popup) => {
+            WA.restorePlayerControls();
+            popup.close();
         }
-    ])
+    }]);
 });
 
-WA.onLeaveZone('ZONETEST', () => {
-    if (popUpExplanation !== undefined) popUpExplanation.close();
-    WA.removeBubble();
-})
+
 
 
 WA.onEnterZone('ZONETEST3', () => {
@@ -90,11 +24,14 @@ WA.onLeaveZone('ZONETEST3', () => {
     WA.sendChatMessage("Goodbye tardif! ", 'Alfred');
 })
 
-var zoneName = "popUpGoToPageZone";
-var urlPricing = "https://workadventu.re/pricing";
-var urlGettingStarted = "https://workadventu.re/getting-started";
-var isCoWebSiteOpened =  false;
-
-WA.onChatMessage((message => {
-    WA.sendChatMessage('Poly Parrot says: "'+message+'"', 'Poly Parrot');
-}));
+WA.onEnterZone('myZone', () => {
+    WA.disablePlayerControls();
+    WA.openPopup("myPopup2", 'NIQUE TOI TA GRAND MERE PUTAIN', [{
+        label: "Got it!",
+        className: "primary",
+        callback: (popup) => {
+            WA.restorePlayerControls();
+            popup.close();
+        }
+    }]);
+});
