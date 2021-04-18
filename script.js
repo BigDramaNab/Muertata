c79c7862ae74c5b4e5de7ed42cb913234b9c975a
johnmetronome <80063998+johnmetronome@users.noreply.github.com>
2021-04-18 13:50:24 +0000

    Deploying to gh-pages from master @ 3c2f5febdc3e7fca62f64e379fed8b793ff6122a 🚀

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-let helloWorldPopup;
-
-// Open the popup when we enter a given zone
-helloWorldPopup = WA.onEnterZone('ZONETEST', () => {
-    WA.openPopup("ZONETEST", 'Hello world!', [{
-        label: "Close",
-        className: "primary",
-        callback: (popup) => {
-            // Close the popup when the "Close" button is pressed.
-            popup.close();
-        }
-    });
-};
-
+WA.sendChatMessage('TEST SCRIPT JS DE 15h49', 'PIVOT');
