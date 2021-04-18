5ce60b0242e3bd22ca1480932ff48188a44d8393
johnmetronome <80063998+johnmetronome@users.noreply.github.com>
2021-04-18 13:16:10 +0000

    Deploying to gh-pages from master @ c8663f1b8a220b23574139940e9827cfacb4ee5d 🚀

---
+++ scripttest.js
@@ -0,0 +1,16 @@
+let helloWorldPopup;
+
+// Open the popup when we enter a given zone
+helloWorldPopup = WA.onEnterZone('ZONETEST', () => {
+    WA.openPopup("ZONETEST", 'Hello world!', [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    });
+};
+
+
+WA.sendChatMessage('Scripttest fonctionne bien', 'Mr Robot');
