c4beab9416e5851de583201932d7296895e927e3
johnmetronome <80063998+johnmetronome@users.noreply.github.com>
2021-04-18 16:06:17 +0000

    Deploying to gh-pages from master @ 00c406cc5abad165a45e2b003e91b54913e54a7d 🚀

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-WA.sendChatMessage('TEST SCRIPT JS DE 17H58', 'PIVOT');
+WA.sendChatMessage('TEST SCRIPT JS DE 18H05', 'PIVOT');
 
 
 WA.onEnterZone('ZONETEST', () => {
     WA.disablePlayerControls();
-    WA.openPopup("myPopup1", 'This is an imporant message!', [{
+    WA.openPopup("myPopup2", 'This is an imporant message!', [{
         label: "Got it!",
         className: "primary",
         callback: (popup) => {
