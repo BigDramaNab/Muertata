d6a1de9a8e4ca157f663b0483a39bb93380ca5d0
johnmetronome <johnmetronome@gmail.com>
2021-04-18 16:14:16 +0200

    1

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-WA.sendChatMessage('TEST SCRIPT JS DE 15h49', 'PIVOT');
+WA.sendChatMessage('TEST SCRIPT JS DE 16h07', 'PIVOT');
+
+let helloWorldPopup;
+
+// Open the popup when we enter a given zone
+helloWorldPopup = WA.onEnterZone('ZONETEST', () => {
+    WA.openPopup("popupRectangle", 'Hello world!', [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    });
+};
+
+// Close the popup when we leave the zone.
+WA.onLeaveZone('ZONETEST', () => {
+    helloWorldPopup.close();
+});
+
+WA.sendChatMessage('TEST numéro2 JS DE 16h07', 'PIVOT');
+
+let helloWorldPopup;
+
+// Open the popup when we enter a given zone
+helloWorldPopup = WA.onEnterZone('ZONETEST2', () => {
+    WA.sendChatMessage('Il voit la zone', 'PIVOT');[{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    });
+};
+
+// Close the popup when we leave the zone.
+WA.onLeaveZone('ZONETEST2', () => {
+    helloWorldPopup.close();
+});
