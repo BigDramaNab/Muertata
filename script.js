42043807bc4ccea25bf3466d391334963a27247e
johnmetronome <80063998+johnmetronome@users.noreply.github.com>
2021-04-18 15:16:21 +0000

    Deploying to gh-pages from master @ cd2f51060fbe7659582505af63f67e97fbdeabaf 🚀

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ WA.onLeaveZone('ZONETEST3', () => {
     WA.sendChatMessage("Goodbye! ZONE TEST 3", 'Mr Robot');
 })
 
+let helloWorldPopup;
+
+// Open the popup when we enter a given zone
+helloWorldPopup = WA.onEnterZone('ZONETEST', () => {
+    WA.openPopup("popupRectangle", 'Hello world!', [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    });
+};
