9e842431e94f318441cf9316f8ea15905b717619
johnmetronome <80063998+johnmetronome@users.noreply.github.com>
2021-04-18 14:17:50 +0000

    Deploying to gh-pages from master @ 41c662a030f405b5e496656a38a754eefa902dab 🚀

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 WA.sendChatMessage('TEST SCRIPT JS DE 16h07', 'PIVOT');
 
+WA.onEnterZone('ZONETEST3', () => {
+    WA.sendChatMessage("Hello! ZONE TEST 3", 'Mr Robot');
+})
+
+WA.onLeaveZone('ZONETEST3', () => {
+    WA.sendChatMessage("Goodbye! ZONE TEST 3", 'Mr Robot');
+})
+
 let helloWorldPopup;
 
 // Open the popup when we enter a given zone
