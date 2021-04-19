1541c69be58a2e5793be116eb6e19d20a31898b3
johnmetronome <80063998+johnmetronome@users.noreply.github.com>
2021-04-19 10:57:42 +0000

    Deploying to gh-pages from master @ 9395e7fa704d66285702591b6f79c584523b61af 🚀

---
+++ scriptZone.js
@@ -0,0 +1,12 @@
+WA.sendChatMessage("Bienvenue! Veuillez suivre le didacticiel afin de profiter de l'experience", 'Alfred');
+
+
+WA.onEnterZone('zoneEchange', () => {
+WA.sendChatMessage("Vous entrez dans la zone d'échange", 'Alfred');
+})
+
+WA.onLeaveZone('ZONETEST3', () => {
+    WA.sendChatMessage("Vous quittez la zone d'échange ", 'Alfred');
+})
+
+
