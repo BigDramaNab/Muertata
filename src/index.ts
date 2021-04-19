b08de5b2c4b9aa4ba81a455c2e79b7678f1e429b
johnmetronome <80063998+johnmetronome@users.noreply.github.com>
2021-04-19 12:08:49 +0000

    Deploying to gh-pages from master @ 5ba2b1bdcb5695d765569caf29d1d1388ce5d3b6 🚀

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-/// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
+/// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.ts" />
 
 console.log('Hello world! From Index! ');
 
