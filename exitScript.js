0c3183ee45b39723e39cefe227c52aae9892fac8
BigDramaNab <79466307+BigDramaNab@users.noreply.github.com>
2021-04-20 18:00:47 +0200

    Add files via upload

--- exitScript.js
+++ exitScript.js
@@ -1,5 +1,3 @@
-WA.sendChatMessage('TEST SCRIPT JS DE 17h45 ET CA CA MARCHE', 'PIVOT');
-
 
 WA.onEnterZone('exitZoneA', () => {
     WA.goToPage('https://www.bigdrama.fr/parfumdefamille/explicationsa');
