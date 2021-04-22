49b2367d68980d436fb390677f43e160b70c39b4
johnmetronome <johnmetronome@gmail.com>
2021-04-22 13:43:19 +0200

    Create scriptTuto.js

---
+++ scriptTuto.js
@@ -0,0 +1,91 @@
+var isFirstTimeTuto = false;
+var textFirstPopup = 'Bonjour, voici comment communiquer, vous pouvez être 4 maximum par bulle';
+var textSecondPopup = 'Vous pouvez aussi utiliser le chat pour communiquer! ';
+var targetObjectTutoBubble ='Tutobubble';
+var targetObjectTutoChat ='tutoChat';
+var targetObjectTutoExplanation ='tutoExplanation';
+var popUpExplanation = undefined;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function launchTuto (){
+    WA.openPopup(targetObjectTutoBubble, textFirstPopup, [
+        {
+            label: "Next",
+            className: "popUpElement",
+            callback: (popup) => {
+                popup.close();
+
+                WA.openPopup(targetObjectTutoChat, textSecondPopup, [
+                    {
+                        label: "Open Chat",
+                        className: "popUpElement",
+                        callback: (popup1) => {
+                            WA.sendChatMessage("Vous pouvez parler ici!", 'La police');
+                            popup1.close();
+                            WA.openPopup("TutoFinal","Vous avez compris",[
+                                {
+                                    label: "Got it!",
+                                    className : "success",callback:(popup2 => {
+                                        popup2.close();
+                                        WA.restorePlayerControl();
+                                    })
+                                }
+                            ])
+                        }
+                    }
+
+                ])
+            }
+        }
+    ]);
+    WA.disablePlayerControl();
+
+}
+
+
+WA.onEnterZone('popupZone', () => {
+    WA.displayBubble();
+    if (!isFirstTimeTuto) {
+        isFirstTimeTuto = true;
+        launchTuto();
+    }
+    else {
+        popUpExplanation = WA.openPopup(targetObjectTutoExplanation, 'Avez vous besoin de revoir les explications?', [
+            {
+                label: "Non",
+                className: "error",
+                callback: (popup) => {
+                    popup.close();
+                }
+            },
+            {
+                label: "Oui",
+                className: "success",
+                callback: (popup) => {
+                    popup.close();
+                    launchTuto();
+                }
+            }
+        ])
+    }
+});
+
+WA.onLeaveZone('popupZone', () => {
+    if (popUpExplanation !== undefined) popUpExplanation.close();
+    WA.removeBubble();
+})
