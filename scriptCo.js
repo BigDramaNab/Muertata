5247d97a02ff54c4d099921a6cc49553fcb34778
BigDramaNab <79466307+BigDramaNab@users.noreply.github.com>
2021-04-23 15:37:57 +0200

    Add files via upload

--- scriptCo.js
+++ scriptCo.js
@@ -10,6 +10,28 @@ var popUpExplanation = undefined;
 
 
 
+WA.onEnterZone('exitZoneA', () => {
+    WA.goToPage('https://www.bigdrama.fr/parfumdefamille/explicationsa');
+})
+
+WA.onEnterZone('exitZoneB', () => {
+    WA.goToPage('https://www.bigdrama.fr/parfumdefamille/explicationsb');
+})
+
+WA.onEnterZone('exitZoneC', () => {
+    WA.goToPage('https://www.bigdrama.fr/parfumdefamille/explicationsc');
+})
+
+WA.onEnterZone('exitZoneD', () => {
+    WA.goToPage('https://www.bigdrama.fr/parfumdefamille/explicationsd');
+})
+
+WA.onEnterZone('exitZoneE', () => {
+    WA.goToPage('https://www.bigdrama.fr/parfumdefamille/explicationse');
+})
+
+
+
 
 function launchTuto (){
     WA.openPopup(targetObjectTutoBubble18, text18, [
