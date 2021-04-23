ef417e8f61b822093e8032ce52cd4e369ca76b7a
johnmetronome <johnmetronome@gmail.com>
2021-04-23 12:18:15 +0200

    OK

---
+++ scriptTuto2.js
@@ -0,0 +1,91 @@
+var isFirstTimeTuto = false;
+var textFirstPopup = 'Bienvenue dans le didacticiel';
+var textSecondPopup = "Vous pouvez parler avec votre équipe dans les bulles (4 max) ";
+var targetObjectTutoBubble ='Tutobubble';
+var targetObjectTutoChat ='tutoChat';
+var targetObjectTutoExplanation ='tutoExplanation';
+var popUpExplanation = undefined;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function launchTuto (){
+    WA.openPopup(targetObjectTutoBubble, textFirstPopup, [
+        {
+            label: "Suite",
+            className: "popUpElement",
+            callback: (popup) => {
+                popup.close();
+
+                WA.openPopup(targetObjectTutoBubble, textSecondPopup, [
+                    {
+                        label: "Suite",
+                        className: "popUpElement",
+                        callback: (popup1) => {
+                            
+                            popup1.close();
+                            WA.openPopup("TutoFinal","N'oubliez pas que les écouteurs sont obligatoires !",[
+                                {
+                                    label: "Compris !",
+                                    className : "success",callback:(popup2 => {
+                                        popup2.close();
+                                        WA.restorePlayerControl();
+                                    })
+                                }
+                            ])
+                        }
+                    }
+
+                ])
+            }
+        }
+    ]);
+    WA.disablePlayerControl();
+
+}
+
+
+WA.onEnterZone('popupZone', () => {
+    WA.displayBubble();
+    if (!isFirstTimeTuto) {
+        isFirstTimeTuto = true;
+        launchTuto();
+    }
+    else {
+        popUpExplanation = WA.openPopup(targetObjectTutoExplanation, 'Avez vous besoin de revoir les explications?', [
+            {
+                label: "Non",
+                className: "error",
+                callback: (popup) => {
+                    popup.close();
+                }
+            },
+            {
+                label: "Oui",
+                className: "success",
+                callback: (popup) => {
+                    popup.close();
+                    launchTuto();
+                }
+            }
+        ])
+    }
+});
+
+WA.onLeaveZone('popupZone', () => {
+    if (popUpExplanation !== undefined) popUpExplanation.close();
+    WA.removeBubble();
+})
